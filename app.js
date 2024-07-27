f54851b684828a56a8ef4b4df7c4ebe376a220bf
MandarAcharekar <mandarmva8055@gmail.com>
2024-07-27 16:56:52 +0530

    routes added

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const dotenv = require('dotenv');
 const express = require('express');
 
-
 dotenv.config();
 
+const authRoutes = require('./routes/authRoutes');
+const preferencesRoutes = require('./routes/preferencesRoutes');
+const newsRoutes = require('./routes/newsRoutes');
+
 const app = express();
-// const port = 3000;
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/users', authRoutes);
+app.use('/users/preferences', preferencesRoutes);
+app.use('/news', newsRoutes);
+
+app.use((err, req, res, next) => {
+    res.status(500).json({ error: err.message });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (err) => {
